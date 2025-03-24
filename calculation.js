8626b5d1787027f974646f02611fe8494ca636d9
Celine Himbaza <celine.himbaza@gmail.com>
2025-03-24 10:10:31 +0200

    Calculation of two numbers

---
+++ calculation.js
@@ -0,0 +1,8 @@
+const a=4
+const b=2
+const sum= a+b;
+const dif= a-b;
+const pro= a*b;
+const div= a/b;
+console.log('the sum of two number is:' +sum + ' ' +'the difference is:' + dif + ' ' +'the product is:' +pro +' ' +'the divisionis:' + div)
+console.log('the sum of two number is:' +sum + ' ' +'the difference is:' + dif + ' ' +'the product is:' +pro +' ' +'the divisionis:' + div)
