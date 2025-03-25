1dc7958a5376597a7ea265d1b0d5b9f90923dfe2
Celine Himbaza <celine.himbaza@gmail.com>
2025-03-25 11:24:59 +0200

    greetings

---
+++ day1.js
@@ -0,0 +1,29 @@
+ // var,let and const
+ var a =3;
+ console.log(a)
+
+ var a =4
+ console.log(a)
+
+ // using var you can redeclare a variable and asign it a value then work
+ let b=3;
+ console.log(b)
+//   let b=6
+//   console.log(b)
+
+  // by let you cant redeclare a variable using the same variable name 
+
+  // Array function
+
+  const add = (a,b) => a+b
+    console.log(add(2,3))
+
+//     function greeting (message){
+//         console.log('hello word')
+//      }
+//   greeting()
+
+     const greeting= (message) => 'hello world'
+     console.log(greeting())
+    
+   
