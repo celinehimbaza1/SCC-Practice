8dac4fd700a6fa356f5dda8b1a1ee117a3e3b507
Celine Himbaza <celine.himbaza@gmail.com>
2025-03-26 14:49:03 +0200

    Filter&Array

--- stack.js
+++ stack.js
@@ -1,11 +1,20 @@
-function first() {
-    console.log("First function");
-    second();
-  }
+// function first() {
+//     console.log("First function");
+//     second();
+//   }
   
-  function second() {
-    console.log("Second function");
-  }
+//   function second() {
+//     console.log("Second function");
+//   }
   
-  first();
-  console.log("End");
+//   first();
+//   console.log("End");
+
+
+let arr=[2,4,6,7]
+// const double = arr.map(a => a*2)
+// console.log(double)
+
+
+const double = arr.filter(a => a % 2==0)
+console.log(double)
