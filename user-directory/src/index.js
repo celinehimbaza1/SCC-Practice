3d75a4e02721eaa7c931301c9d17f3d4d8ac5487
celine <celine.himbaza@gmail.com>
2025-05-06 10:12:00 +0200

    index.js

--- user-directory/src/index.js
+++ user-directory/src/index.js
@@ -1,6 +1,6 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import App from "./App";
-import "./index.css"; // Optional: Tailwind CSS or other styles
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
 
-ReactDOM.render(<App />, document.getElementById("root"));
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(<App />);
